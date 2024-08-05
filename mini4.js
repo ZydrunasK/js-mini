9a600877649b925dd247409f6415d29df26e568b
Zydrunask <kniukstaz@gmail.com>
2024-08-05 10:49:49 +0300

    suma intervale

--- mini4.js
+++ mini4.js
@@ -178,3 +178,91 @@ console.log();
 console.log(floatTxt2(-18, 18, div5c));
 console.log();
 console.log(floatTxt3(-18, 18, div7c));
+
+console.clear();
+
+// -----------darbas pamokoje------------
+
+
+function sumaIntervale(a, b) {
+  // 1. validation of information (params)
+  if (typeof a !== 'number') {
+    return 'ERROR: pirma reiksme turi buti skaicius';
+  }
+  if (a === Infinity || a === -Infinity) {
+    return 'ERROR: biski perdaug nori';
+  }
+  if (isNaN(a)) {
+    return 'ERROR: pirma reiksme turi buti skaicius';
+  }
+  if (a % 1 !== 0) {
+    return 'ERROR: pirma reiksme turi buti sveikas skaicius';
+  }
+
+  if (typeof b !== 'number') {
+    return 'ERROR: antra reiksme turi buti skaicius';
+  }
+  if (b === Infinity || b === -Infinity) {
+    return 'ERROR: biski perdaug nori';
+  }
+  if (isNaN(b)) {
+    return 'ERROR: pirma reiksme turi buti skaicius';
+  }
+  if (b % 1 !== 0) {
+    return 'ERROR: antra reiksme turi buti sveikas skaicius';
+  }
+
+  if (a > b) {
+    return 'ERROR: pirma reiksme turi buti didesne uz antra';
+  }
+  // 2. logic
+    let rez = 0;
+    for (let i = a; i <= b; i++) {
+      rez += i;
+    }
+
+  // 3. results of logic validation
+    if (a % 1 !== 0) {
+      return 'ERROR: '
+    }
+  // 4. result
+  return rez;
+
+}
+
+console.log(sumaIntervale(0, 0));
+console.log(sumaIntervale(0, 4));
+console.log(sumaIntervale(0, 100));
+console.log(sumaIntervale(574, 815));
+console.log(sumaIntervale(-50, 50));
+console.log(sumaIntervale(-70, 30));
+console.log(sumaIntervale(-70, 30,));
+console.log(sumaIntervale(0, NaN));
+console.log(sumaIntervale(0, Infinity));
+console.log(sumaIntervale(Infinity, 5));
+
+console.log(sumaIntervale(0, '0'));
+console.log(sumaIntervale('0', 0));
+console.log(sumaIntervale(777, true));
+console.log(sumaIntervale(true, 777));
+console.log(sumaIntervale(true, false));
+console.log(sumaIntervale([], 777));
+console.log(sumaIntervale([], [777]));
+console.log(sumaIntervale([], {}));
+console.log(sumaIntervale({}, [777]));
+console.log(sumaIntervale(null, null));
+console.log(sumaIntervale(undefined, 12));
+console.log(sumaIntervale(sumaIntervale, 12));
+
+console.log(sumaIntervale(10, NaN));
+console.log(sumaIntervale(NaN, 10));
+
+console.log(sumaIntervale());
+console.log(sumaIntervale(10));
+
+console.log(sumaIntervale(10.2, 14,6));
+console.log(sumaIntervale(10 , 5));
+
+
+
+
